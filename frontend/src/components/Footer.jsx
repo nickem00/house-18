5a765d14fdb144d4e1786aa9f8abd2c062bb6c71
nickem00 <nickemalm00@gmail.com>
2025-05-03 17:53:50 +0200

    Added basic structure in frontend. Now renders basic structure at localhost:5173. Added folder/file structure.

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+      <footer>
+        <p style={{ textAlign: "center", padding: "1rem" }}>
+          &copy; {new Date().getFullYear()} House 18. Alla rättigheter förbehållna.
+        </p>
+      </footer>
+    );
+  }
+  
