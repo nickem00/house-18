c3210439484f3158b5219c440a68ee64daa10740
nickem00 <nickemalm00@gmail.com>
2025-05-06 21:01:14 +0200

    Added content to hero-section in Home-page

--- frontend/src/components/Home/hero-section.jsx
+++ frontend/src/components/Home/hero-section.jsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom"
+import '../../styles/home.css'
+
 export default function HeroSection() {
     return (
         <section className="hero-section">
-            <h1>Hero Section</h1>
+            <div className="hero-content">
+                <div className="hero-text-div">
+                    <h1 className="hero-text">More than things.</h1>
+                    <h1 className="hero-text">A way of living.</h1>
+                    <Link className="shop-now-button" to="/store">SHOP NOW</Link>
+                </div>
+                <div className="hero-image">
+                    <img src="https://res.cloudinary.com/dzcqnchjm/image/upload/v1746556423/shirt-hero-section_b7rsnf.png" alt="Hero" />
+                </div>
+            </div>
         </section>
     )
 }
