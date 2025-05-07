0dd3c556a196013db3ce0fe659f0190d537f2fb7
nickem00 <nickemalm00@gmail.com>
2025-05-07 13:07:33 +0200

    Added about section

--- frontend/src/components/Home/about-store-section.jsx
+++ frontend/src/components/Home/about-store-section.jsx
@@ -2,8 +2,27 @@ import '../../styles/home.css'
 
 export default function AboutStoreSection() {
     return (
-        <section className='about-store-section'>
-            <h2>About the store</h2>
+        <section className='about-store-section' id='about-store'>
+            <h2 className='about-store-title'>ABOUT THE STORE</h2>
+            <p className='about-store-text'>House 18 is more than a store — it's a philosophy of curated essentials, rich textures, and timeless tones.</p>
+            <div className='about-store-image-container'>
+                <div className="about-store-image-card">
+                    <img src="https://res.cloudinary.com/dzcqnchjm/image/upload/v1746553301/klim-musalimov-RkMX-abF_IQ-unsplash_wuyssi.jpg" alt="Background image of building" />
+                    <div className='about-store-image-overlay' />
+                    <div className="about-store-image-text">
+                        <p>Timeless Essentials</p>
+                        <p>Designed to elevate you</p>
+                    </div>
+                </div>
+                <div className="about-store-image-card">
+                    <img src="https://res.cloudinary.com/dzcqnchjm/image/upload/v1746553303/wes-hicks-7r6mdIcQeKk-unsplash_kzeawh.jpg" alt="Background image of building" />
+                    <div className='about-store-image-overlay' />
+                    <div className="about-store-image-text">
+                        <p>Quality First</p>
+                        <p>We curate what lasts</p>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
