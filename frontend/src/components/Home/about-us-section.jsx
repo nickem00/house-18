081d32a63b7db16b78f9caecc4cb26f1a653d838
nickem00 <nickemalm00@gmail.com>
2025-05-07 13:34:35 +0200

    Added 'about us'-section.

--- frontend/src/components/Home/about-us-section.jsx
+++ frontend/src/components/Home/about-us-section.jsx
@@ -1,9 +1,32 @@
 import '../../styles/home.css'
+import TeamCard from './TeamCard'
 
-export default function HeroSection() {
+export default function AboutUsSection() {
     return (
-        <section>
-            <h2>About Us</h2>
+        <section className='about-us-section' id='about-us'>
+            <h2 className='about-us-title'>THE PEOPLE BEHIND HOUSE 18</h2>
+            <div className="about-us-image-container">
+                <TeamCard 
+                    name="Nicholas" 
+                    role="Fullstack Integration Specialist" 
+                    image="https://res.cloudinary.com/dzcqnchjm/image/upload/v1746616899/Screenshot_2025-05-03_161625_qhi71y.png" 
+                />
+                <TeamCard 
+                    name="Hugo" 
+                    role="Feature Developer Coordinator" 
+                    image="https://res.cloudinary.com/dzcqnchjm/image/upload/v1746616899/Screenshot_2025-05-02_182649_vwyavk.png" 
+                />
+                <TeamCard 
+                    name="Jacob" 
+                    role="Technical Implementation Generalist" 
+                    image="https://res.cloudinary.com/dzcqnchjm/image/upload/v1746616899/Screenshot_2025-05-03_161953_upzejn.png" 
+                />
+                <TeamCard 
+                    name="Pontus" 
+                    role="Bi-odlare" 
+                    image="https://res.cloudinary.com/dzcqnchjm/image/upload/v1746616900/Screenshot_2025-05-03_161505_ayirj2.png" 
+                />
+            </div>
         </section>
     )
 }
