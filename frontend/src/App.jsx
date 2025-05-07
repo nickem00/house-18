36bd39d83186fedadf6335fde7a2548487242c04
nickem00 <nickemalm00@gmail.com>
2025-05-07 12:39:18 +0200

    minor fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Header from "./components/Header"; // Placeholder
-import Footer from "./components/Footer"; // Placeholder
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,44 +8,46 @@ export default function Header() {
 
     return (
         <header>
-            <nav>
-                <ul>
-                    <li><Link to="/about">ABOUT US</Link></li>
-                    <li><Link to="/contact">CONTACT</Link></li>
-                </ul>
-            </nav>
+            <div className="container header-content">
+                <nav>
+                    <ul>
+                        <li><Link to="/about">ABOUT US</Link></li>
+                        <li><Link to="/contact">CONTACT</Link></li>
+                    </ul>
+                </nav>
 
-            <h1 className="logo"><Link to="/">HOUSE 18</Link></h1>
+                <h1 className="logo"><Link to="/">HOUSE 18</Link></h1>
 
-            <nav>
-                <ul>
-                    <li><Link to="/store">STORE</Link></li>
-                    <li>
-                        <ul className="icons-list">
-                            <li>
-                                <Link className="icon" to="/profile">
-                                    <FontAwesomeIcon icon={['far', 'user']} />
-                                </Link>
-                            </li>
-                            <li>
-                                <Link className="icon" to="/cart">
-                                    <FontAwesomeIcon icon={['fas', 'cart-shopping']} />
-                                </Link>
-                            </li>
-                            <li>
-                                <Link
-                                 className="icon"
-                                 to="/wishlist"
-                                 onMouseEnter={() => setIsHeartHovered(true)}
-                                 onMouseLeave={() => setIsHeartHovered(false)}
-                                 >
-                                    <FontAwesomeIcon icon={isHeartHovered ? ['fas', 'heart'] : ['far', 'heart']} />
-                                </Link>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </nav>
+                <nav>
+                    <ul>
+                        <li><Link to="/store">STORE</Link></li>
+                        <li>
+                            <ul className="icons-list">
+                                <li>
+                                    <Link className="icon" to="/profile">
+                                        <FontAwesomeIcon icon={['far', 'user']} />
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link className="icon" to="/cart">
+                                        <FontAwesomeIcon icon={['fas', 'cart-shopping']} />
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        className="icon"
+                                        to="/wishlist"
+                                        onMouseEnter={() => setIsHeartHovered(true)}
+                                        onMouseLeave={() => setIsHeartHovered(false)}
+                                    >
+                                        <FontAwesomeIcon icon={isHeartHovered ? ['fas', 'heart'] : ['far', 'heart']} />
+                                    </Link>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     );
 }
