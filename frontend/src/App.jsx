fb2f6f8beb84dcddd2b61ae950d337e519b273ac
nickem00 <nickemalm00@gmail.com>
2025-05-09 00:36:55 +0200

    Fixed cart-modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,16 @@ import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ScrollToHashElement from "./features/ScrollToHashElement";
+import CartModal from "./components/CartModal";
+import { useState } from "react";
 
 export default function App() {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  
   return (
     <>
-      <Header />
+      <Header setIsCartOpen={setIsCartOpen} isCartOpen={isCartOpen} />
+      <CartModal isOpen={isCartOpen} onClose={() => setIsCartOpen(false)} />
       <ScrollToHashElement />
       <main>
         <Outlet />
