b71439ecbe30e838c4b9cc3e03d6f732c2244916
Jagge1 <hellgrenjacob@hotmail.com>
2025-05-12 23:25:20 +0200

    Add to cart button on product page now works and the size options now dynamically display sizes based on stock. As well as some minor changes to styling

--- frontend/src/components/ProductPage/ProductInfo.jsx
+++ frontend/src/components/ProductPage/ProductInfo.jsx
@@ -1,19 +1,56 @@
+import { useCart } from "../../context/useCart"
+import { useState } from "react";
+
 export default function ProductInfo({product}){
+
+    const { addToCart } = useCart();
+    const [selectedSize, setSelectedSize] = useState("");
+
     return(
         <section className="product-info">
             <h4 className="product-title">{product.name}</h4>
             <p>{product.price}kr</p>
 
             <section className="product-actions"  defaultValue="SIZE">
-                <select className="size-menu" name="sizes" id="sizes">
+                <select
+                 className="size-menu"
+                 name="sizes" id="sizes"
+                 onChange={(e) => setSelectedSize(e.target.value)}
+                 value={selectedSize}>
                 <option value="" disabled selected>SIZE</option>
-                <option value="XS">XS</option>
-                <option value="S">S</option>
-                <option value="M">M</option>
-                <option value="L">L</option>
-                <option value="XL">XL</option>
+                {product.variants.map((variant)=> (
+                    <option 
+                    key={variant._id}
+                    value={variant.size}
+                    disabled={variant.stock === 0}
+                    
+                    >
+                        {variant.size} {variant.stock === 0 ? "(out of stock)" : ""}
+                    </option>
+                ))}
             </select>
-            <button className="add-to-cart-btn">ADD TO CART</button>
+
+            <button className="add-to-cart-btn" 
+            onClick={() => {
+
+                if (!selectedSize) {
+                    alert("You have to choose a size before adding to cart")
+                    return;
+                }
+                
+                const cartItem = {
+                product_id: product.product_id,
+                name: product.name,
+                price: product.price,
+                image: product.images[0],
+                size: selectedSize, 
+                quantity: 1
+                }
+                
+                addToCart(cartItem);
+                setSelectedSize("");
+                
+                }} >ADD TO CART</button>
             </section>
 
             <ul className="shipping-and-details">
